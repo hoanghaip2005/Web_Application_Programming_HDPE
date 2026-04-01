1f9f54a97f0733ec1e01f9a9faf7fdaf00c58673
hoanghaip2005 <hoanghaip2005@gmail.com>
2026-04-02 00:13:30 +0700

    Khởi tạo base

---
+++ frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://localhost:4443",
+        changeOrigin: true,
+        secure: false
+      },
+      "/api-docs": {
+        target: "https://localhost:4443",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+});
